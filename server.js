d21e716fb0aa102e7eb43b39993ff196e31a1247
Damerian Dark <newbmailz@gmail.com>
2021-09-03 18:37:49 -0500

    set up dependencies

---
+++ server.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.emv.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the movies_db database.`)
+);
