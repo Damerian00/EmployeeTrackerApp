c49f4c8f7942082ab52df00e9b6b4e83fe90d595
Damerian Dark <newbmailz@gmail.com>
2021-09-04 16:15:37 -0500

    Have the first set of questions set up alopng with a switch to catch the answers and redirect to functions

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const inquirer = require('inquirer');
+
+
+
+
 
 init();
 
@@ -25,6 +30,69 @@ console.log(
 `
 );
 
-
+askQuestions();
 
  }
+
+
+ function askQuestions (){
+    const questions = [
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'What would you like to do? :',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role',
+             'View all Roles', 'Add Role', 'View All Departments', 'Add Department',
+            'Quit']
+        },
+       
+    ];
+    inquirer.prompt(questions).then((answers) => {
+       moreQuestions(answers);  
+ });
+        
+}
+
+function moreQuestions(answers){
+    switch (answers.mainMenu) {
+        case 'Add Employee':
+          addEmployee(answers);
+            break;
+        case 'Update Employee Role':
+          updateEmployee(answers);  
+            break;    
+        case 'Add Role':
+           addRole(answers);
+            break;    
+        case 'Add Department':
+            addDepartment(answers); 
+        break;    
+     
+        default:
+            break;
+    }
+    if (answers.mainMenu === "Quit"){
+        
+    }else{
+        askQuestions();
+
+    }
+}
+
+function addEmployee (answers){
+    let tempObject = answers;
+    console.log(tempObject);
+}
+
+function updateEmployee (answers){
+    let tempObject = answers;
+    console.log(tempObject);
+}
+function addRole(answers){
+    let tempObject = answers;
+    console.log(tempObject);
+}
+function addDepartment(answers){
+    let tempObject = answers;
+    console.log(tempObject);
+}
