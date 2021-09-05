1c8a91cf7edaca6ea5af67e87b6a86c046d43a64
Damerian Dark <newbmailz@gmail.com>
2021-09-05 15:25:14 -0500

    have the add function pulling from dbinquirer

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,14 @@
-SELECT employees.id, firstName as First_Name, lastName as Last_Name, roles.title as Title, departments.name as Department, roles.salary, managers.name as Manager_Name
-FROM employees
-INNER JOIN roles ON roles.id = employees.role_id 
-INNER JOIN departments ON departments.id = employees.department_id
-INNER JOIN managers ON managers.id = employees.manager_id
-ORDER BY employees.id;
+-- SELECT employees.id, firstName as First_Name, lastName as Last_Name, roles.title as Title, departments.name as Department, roles.salary, managers.name as Manager_Name
+-- FROM employees
+-- INNER JOIN roles ON roles.id = employees.role_id 
+-- INNER JOIN departments ON departments.id = employees.department_id
+-- INNER JOIN managers ON managers.id = employees.manager_id
+-- ORDER BY employees.id;
+
+
+-- SELECT title as role
+--        FROM roles
+--        ORDER BY role;
+
+SELECT name
+    FROM managers;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 // const db = require('./server');
 const sql = require('./index');
+let empRoleArr = [];
+let empMgrArr = [];
 /*|
   */
 const mysql = require('mysql2');
@@ -114,7 +116,7 @@ function moreQuestions(answers){
 
         switch (answers.mainMenu) {
             case 'Add Employee':
-              addEmployee(answers);
+              addEmployee();
                 break;
             case 'Update Employee Role':
               updateEmployee(answers);  
@@ -126,17 +128,17 @@ function moreQuestions(answers){
                 addDepartment(answers); 
             break;
             case 'View All Employees':    
-                viewEmployees();
-           
+            viewEmployees();
+            
             break;
             default:
-
-               break;
+                
+                break;
             }
-           
-
-    }
-    db.end();
+            
+            
+        }
+        
 
 }
 
@@ -153,61 +155,106 @@ function viewEmployees (){
 
            return;
          }
-         console.log(' ');
-         console.table(rows);
-         console.log(' ');
+          
+         
 
+         console.log(' ');
+        console.log(` 
+id  first_name      last_name      title               department                    salary         manager
+--- -------------  --------------  ------------------  --------------------------    -------------- ------------------
+         `);
+        rows.forEach( (row) =>{
+           console.log(`
+${row.id}    ${row.First_Name}          ${row.Last_Name}            ${row.Title}             ${row.Department}              ${row.salary}           ${row.Manager_Name}`);
+        }) 
+        console.log(' ');
+            
 
        });
-       askQuestions();
+        askQuestions();
    }
-function addEmployee (menuChoice){
-
-    let tempObject = menuChoice;
+function addEmployee (){
+    let sql = `SELECT title as role
+    FROM roles
+    ORDER BY role;`;  
+       db.query(sql, (err, rows) => {
+         if (err) {
+             console.log(err);
+             
+             return;
+            }
+            rows.forEach( (row) =>{
+                empRoleArr.push(row.role);
+            }) 
+        })
+        sql = `SELECT name as managerName FROM managers;`;  
+           db.query(sql, (err, rows) => {
+             if (err) {
+                 console.log(err);
+                 
+                 return;
+                }
+                rows.forEach( (row) =>{
+                    empMgrArr.push(row.managerName);
+                }) 
+            })
     
-    const questions = [
-        {
-            type: 'input',
-            name: 'firstName',
-            message: 'What is the Employee\'s first name? :',
-            validate(value) {
-                const fails = value.match(
-                  /([0-9])/i
-                );
-                if (fails || value === "") {
-                    return 'Please enter in a valid name.';
-                 
-                }
-                    return true;
-              },
-              filter(value) {
-                value.toLowerCase();
-                return value.charAt(0).toUpperCase() + value.slice(1);
-              },
+        const questions = [
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the Employee\'s first name? :',
+                validate(value) {
+                    const fails = value.match(
+                        /([0-9])/i
+                        );
+                        if (fails || value === "") {
+                            return 'Please enter in a valid name.';
+                            
+                        }
+                        return true;
+                    },
+                    filter(value) {
+                        value.toLowerCase();
+                        return value.charAt(0).toUpperCase() + value.slice(1);
+                    },
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'What is the Employee\'s last name? :',
+                    validate(value) {
+                        const fails = value.match(
+                            /([0-9])/i
+                            );
+                            if (fails || value === "") {
+                                return 'Please enter in a valid name.';
+                                
+                            }
+                            return true;
+                        },
+                        filter(value) {
+                            value.toLowerCase();
+                            return value.charAt(0).toUpperCase() + value.slice(1);
+                        },
         },
         {
-            type: 'input',
-            name: 'lastName',
-            message: 'What is the Employee\'s last name? :',
-            validate(value) {
-                const fails = value.match(
-                  /([0-9])/i
-                );
-                if (fails || value === "") {
-                    return 'Please enter in a valid name.';
-                 
-                }
-                    return true;
-              },
-              filter(value) {
-                  value.toLowerCase();
-                return value.charAt(0).toUpperCase() + value.slice(1);
-              },
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What is the Employee\'s role? :',
+            choices: empRoleArr,
         },
+        {
+            type: 'list',
+            name: 'employeeMgr',
+            message: 'Who is the Employee\'s manager? Use self if they are the manager. :',
+            choices: empMgrArr,
+        },
+        
     ];
     inquirer.prompt(questions).then((answers) => {
         console.log(`Added ${answers.firstName} ${answers.lastName} to the databse.`);
-        askQuestions();    
+            
     });
 }
 
@@ -254,20 +301,20 @@ function updateEmployee (menuChoice){
         },
     ];
     inquirer.prompt(questions).then((answers) => {
-        console.log(`Added ${answers.firstName} ${answers.lastName} to the databse.`);
-        askQuestions();     
+        console.log(`Updated ${answers.firstName} ${answers.lastName} the databse.`);
+        // askQuestions();     
     });
    
 }
 function addRole(menuChoice){
     let tempObject = menuChoice;
     console.log(`Added to the database.`);
-    askQuestions();
+    // askQuestions();
 }
 function addDepartment(menuChoice){
     let tempObject = menuChoice;
     console.log(`Addded to the database.`);
-    askQuestions();
+    // askQuestions();
 }
 
 module.exports = sql
